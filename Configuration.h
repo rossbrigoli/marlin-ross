8cfafcbb9c1d3d8bb386727c469fde0a19e7530b
Ross Brigoli <brigoli@gmail.com>
2021-03-22 18:15:10 +0800

    Verison 3.0 - Replaced with MKS Gen L and TMC 2208

--- Configuration.h
+++ Configuration.h
@@ -28,7 +28,7 @@
 // 51 = Gen6 deluxe
 // 6  = Sanguinololu < 1.2
 // 62 = Sanguinololu 1.2 and above
-// 63 = Melzi
+// 63 = MelziJBOS
 // 64 = STB V1.1
 // 65 = Azteeg X1
 // 66 = Melzi with ATmega1284 (MaKr3d version)
@@ -53,11 +53,11 @@
 // 21 = Elefu Ra Board (v3)
 
 #ifndef MOTHERBOARD
-#define MOTHERBOARD 33 
+#define MOTHERBOARD 34 
 #endif
 
 
-#define CUSTOM_MENDEL_NAME "ROSS v2.8"
+#define CUSTOM_MENDEL_NAME "ROSS v3.0"
 
 // Define this to set a custom name for your generic Mendel,
 // #define CUSTOM_MENDEL_NAME "This Mendel"
@@ -308,12 +308,12 @@ const bool Z_MAX_ENDSTOP_INVERTING = false;
 #define DISABLE_Z false
 #define DISABLE_E false // For all extruders
 
-#define INVERT_X_DIR true    // for Mendel set to false, for Orca set to true
-#define INVERT_Y_DIR true    // for Mendel set to true, for Orca set to false
-#define INVERT_Z_DIR true    // for Mendel set to false, for Orca set to true
-#define INVERT_E0_DIR false   // for direct drive extruder v9 set to true, for geared extruder set to false
-#define INVERT_E1_DIR false   // for direct drive extruder v9 set to true, for geared extruder set to false
-#define INVERT_E2_DIR false   // for direct drive extruder v9 set to true, for geared extruder set to false
+#define INVERT_X_DIR false    // for Mendel set to false, for Orca set to true
+#define INVERT_Y_DIR false    // for Mendel set to true, for Orca set to false
+#define INVERT_Z_DIR false    // for Mendel set to false, for Orca set to true
+#define INVERT_E0_DIR true   // for direct drive extruder v9 set to true, for geared extruder set to false
+#define INVERT_E1_DIR true   // for direct drive extruder v9 set to true, for geared extruder set to false
+#define INVERT_E2_DIR true   // for direct drive extruder v9 set to true, for geared extruder set to false
 
 // ENDSTOP SETTINGS:
 // Sets direction of endstops when homing; 1=MAX, -1=MIN
